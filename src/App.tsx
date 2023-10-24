d405c54e2b05afe0b7e75a5bb6a4fc4f00e23709
Maltsau <emeraldwhistler.dm@gmail.com>
2023-10-24 19:57:17 +0300

    feat: fetch data init

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,18 @@ import './App.css';
 import { Component, ReactNode } from 'react';
 
 class App extends Component {
+  state = {
+    url: 'https://swapi.dev/api/people/',
+    data: null,
+  };
+
+  componentDidMount(): void {
+    fetch(this.state.url)
+      .then((response) => response.json())
+      .then((data) => this.setState({ data }));
+    console.log(this.state.data);
+  }
+
   render(): ReactNode {
     return <>Hello</>;
   }
