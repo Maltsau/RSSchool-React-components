2a5ef9e28d202f5ef7d1be667db1f7b7f4fa9f12
Maltsau <emeraldwhistler.dm@gmail.com>
2023-11-23 07:53:07 +0300

    config: environment config

---
+++ pages/_app.tsx
@@ -0,0 +1,35 @@
+import type { AppProps } from 'next/app';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`                              
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+interface ThemeInterface {
+  colors: {
+    primary: string;
+    backgroundPrimary: string;
+  };
+}
+
+const theme: ThemeInterface = {
+  colors: {
+    primary: '#fffff',
+    backgroundPrimary: 'black',
+  },
+};
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
